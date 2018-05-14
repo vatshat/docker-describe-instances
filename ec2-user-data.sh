12d7ee4c97a38ceaf28de23cb06a6f2d50c46245
vatshat <36160875+vatshat@users.noreply.github.com>
2018-05-14 19:30:55 +0200

    Updating temporary credentials

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -21,17 +21,23 @@ cd /home/ec2-user/bsc-repo/docker-describe-instances
 
 # get the credentials from isntance role
 
-# Alternative to using jq - https://michaelheap.com/extract-substring-values-jq
+# sed...alternative to using jq - https://michaelheap.com/extract-substring-values-jq
 # instance_role=curl -s 169.254.169.254/latest/meta-data/iam/info | jq ".InstanceProfileArn" | sed -e 's/^"//' -e 's/"$//' | awk -F'/' '{print $2}'
 
 # https://vsupalov.com/docker-arg-env-variable-guide/
 # https://github.com/docker/compose/issues/2111#issuecomment-174571609
 
-instance_role=$(curl -s http://169.254.169.254/latest/meta-data/iam/info | jq -r '. | map(.)[2] | split("/")[1]')
-access_key_id=$(curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/"$instance_role"); access_key_id=$(jq --raw-output ".AccessKeyId" <<< $access_key_id)
-secret_access_key=$(curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/"$instance_role"); secret_access_key=$(jq --raw-output ".SecretAccessKey" <<< $secret_access_key)
-aws_default_region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/.$//')
+service docker start
 
-docker-compose build --build-arg access_key_id=$access_key_id --build-arg secret_access_key=$secret_access_key --build-arg region=$aws_default_region
-docker-compose -f docker-compose.local.yml up -d --force-recreate
-docker-compose up -d --force-recreate
+instance_role=$(curl -s http://169.254.169.254/latest/meta-data/iam/info | jq -r '. | map(.)[2] | split("/")[1]')
+
+access_key_id=$(curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/"$instance_role"); access_key_id=$(jq --raw-output ".AccessKeyId" <<< $access_key_id)
+export AWS_ACCESS_KEY_ID=$access_key_id
+secret_access_key=$(curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/"$instance_role"); secret_access_key=$(jq --raw-output ".SecretAccessKey" <<< $secret_access_key)
+export AWS_SECRET_ACCESS_KEY=$secret_access_key
+session_token=$(curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/"$instance_role"); session_token=$(jq --raw-output ".Token" <<< $session_token)
+export AWS_SESSION_TOKEN=$session_token
+
+/usr/local/bin/docker-compose build --build-arg access_key_id=$access_key_id --build-arg secret_access_key=$secret_access_key --build-arg aws_default_region=$aws_default_region --build-arg session_token=$session_token
+/usr/local/bin/docker-compose -f docker-compose.local.yml up -d --force-recreate
+/usr/local/bin/docker-compose up -d --force-recreate
